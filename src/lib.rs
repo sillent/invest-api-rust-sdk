3fec8529973396350b5dab2da981b8e7d1ed1f15
Dmitry Ulyanov <sillent1987@gmail.com>
2025-02-22 23:10:04 +0300

    proto data use in lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,31 @@
-pub(crate) mod investment_api {
-    tonic::include_proto!("tinkoff.public.invest.api.contract.v1");
+pub mod prelude;
+use std::time;
+
+pub use prelude::investment_api::*;
+
+pub const PROD_ENDPOINT: &'static str = "https://invest-public-api.tinkoff.ru:443";
+pub const SANDBOX_ENDPOINT: &'static str = "https://sandbox-invest-public-api.tinkoff.ru:443";
+
+
+pub struct PoolServices {
+    inner: tonic::transport::Endpoint,
 }
 
-pub use investment_api::*;
+
+impl PoolServices {
+    pub fn new() -> PoolServices {
+        let tls = tonic::transport::channel::ClientTlsConfig::new().with_native_roots();
+        let endpoint = tonic::transport::Channel::from_static(SANDBOX_ENDPOINT)
+            .tls_config(tls)
+            .expect("tls configuration error")
+            .rate_limit(200, time::Duration::from_secs(1));
+
+        PoolServices { inner: endpoint }
+    }
+
+    pub async fn user_service(
+        &self,
+    ) -> orders_service_client::OrdersServiceClient<tonic::transport::Channel> {
+        unimplemented!();
+    }
+}
---
+++ src/prelude.rs
@@ -0,0 +1,3 @@
+pub(crate) mod investment_api {
+    tonic::include_proto!("tinkoff.public.invest.api.contract.v1");
+}
