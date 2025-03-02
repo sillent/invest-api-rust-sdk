03e755ff91dd7a1a4a4c930b39ad1a8dadd3b59f
Dmitry Ulyanov <sillent1987@gmail.com>
2025-03-02 14:32:04 +0300

    user_client impl

--- src/other.rs
+++ src/other.rs
@@ -1,21 +1,33 @@
 use std::time::Duration;
 use tonic::metadata::{MetadataMap, MetadataValue};
 use tonic::service::interceptor::InterceptedService;
-use tonic::transport::{Channel, ClientTlsConfig, Endpoint};
-use tonic::{Request, Status};
+use tonic::transport::{Channel, ClientTlsConfig};
+use tonic::Request;
 
 use crate::investment_api::users_service_client::UsersServiceClient;
 
-pub struct GrpcClientFactory {
+
+pub struct ServiceConfig {
+    pub base_url: String,
+    pub auth_token: String,
+    pub user_agent: Option<String>,
+    pub headers: Option<(&'static str, String)>,
+}
+
+pub struct Ready;
+pub struct NotReady;
+
+pub struct GrpcClientFactory<State = NotReady> {
     base_url: String,
     auth_token: String,
     tls_config: ClientTlsConfig,
     channel: Option<Channel>,
     metadata: MetadataMap,
+    state: std::marker::PhantomData<State>
 }
 
-impl GrpcClientFactory {
-    pub fn new(base_url: String, auth_token: String) -> Self {
+impl GrpcClientFactory<NotReady> {
+    pub fn new(base_url: String, auth_token: String) -> GrpcClientFactory<NotReady> {
         let tls_config = ClientTlsConfig::new().with_native_roots();
         Self {
             base_url,
@@ -23,32 +35,54 @@ impl GrpcClientFactory {
             tls_config,
             channel: None,
             metadata: MetadataMap::new(),
+            state: std::marker::PhantomData::<NotReady>,
         }
     }
 
     pub async fn connect(
-        &mut self,
+        self,
         timeout: Duration,
         rate_limit: (u64, Duration),
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let token: MetadataValue<_>= format!("Bearer {}", self.auth_token).parse()?;
+    ) -> Result<GrpcClientFactory<Ready>, Box<dyn std::error::Error>> {
+        // let token: MetadataValue<_>= format!("Bearer {}", self.auth_token).parse()?;
         let channel = Channel::from_shared(self.base_url.clone())?
             .tls_config(self.tls_config.clone())?
             .rate_limit(rate_limit.0, rate_limit.1)
             .timeout(timeout)
             .connect()
             .await?;
-        self.channel = Some(channel);
-        Ok(())
+        Ok(GrpcClientFactory {
+            base_url: self.base_url,
+            auth_token: self.auth_token,
+            tls_config: self.tls_config,
+            metadata: self.metadata,
+            channel: Some(channel),
+            state: std::marker::PhantomData::<Ready>,
+        })
+        // self.channel = Some(channel);
+
+        // Ok(self.
     }
 
 
     pub async fn user_service(&self, channel: Channel) -> UsersServiceClient<InterceptedService<Channel, impl FnMut(Request<()>) -> Result<Request<()>, tonic::Status>>> {
         let token: MetadataValue<_>= format!("Bearer {}", self.auth_token).parse().expect("parsing correct");
-         UsersServiceClient::with_interceptor(channel, move |mut req: Request<()>| {
+         UsersServiceClient::with_interceptor(channel.clone(), move |mut req: Request<()>| {
             req.metadata_mut().insert("authorization", token.clone());
             Ok(req)
         })
     }
 
+}
+
+impl GrpcClientFactory<Ready> {
+    pub fn user_service(&self) -> UsersServiceClient<InterceptedService<Channel, impl FnMut(Request<()>) -> Result<Request<()>, tonic::Status>>> {
+        let token: MetadataValue<_>= format!("Bearer {}", self.auth_token).parse().expect("parsing correct");
+        let channel = self.channel.clone();
+         UsersServiceClient::with_interceptor(channel.unwrap(), move |mut req: Request<()>| {
+            req.metadata_mut().insert("authorization", token.clone());
+            Ok(req)
+        })
+        
+    }
 }
