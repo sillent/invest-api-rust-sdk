b27e37d78d4447ad09b2fcfa6bb5b13fc4ba8f9d
Dmitry Ulyanov <sillent1987@gmail.com>
2026-05-24 10:32:33 +0300

    add mor examples (#10)
    * users_service more examples
    
    * user serivce - add example bank_account
    
    * add example order_stream_service
    
    * more examples

--- examples/orders_service.rs
+++ examples/orders_service.rs
@@ -4,14 +4,20 @@ use invest_api_rust_sdk::{ServiceFactory, SANDBOX_ENDPOINT};
 
 #[tokio::main]
 async fn main() {
-	let token_string = env::var("TOKEN").expect("TOKEN environment");
-	let factory = ServiceFactory::builder()
-		.base_url(SANDBOX_ENDPOINT)
-		.token(token_string)
-		.user_agent("in-rust-we-trust")
-		.build()
-		.unwrap();
-	let mut orders_sv = factory.orders_service();
-	let orders = orders_sv.get_orders(invest_api_rust_sdk::contracts::GetOrdersRequest{account_id: "xxx".to_owned()}).await;
-	println!("{orders:#?}");
+    let token_string = env::var("TOKEN").expect("TOKEN environment");
+    let account_id = env::var("ACCOUNT_ID").unwrap_or_default();
+    let factory = ServiceFactory::builder()
+        .base_url(SANDBOX_ENDPOINT)
+        .token(token_string)
+        .user_agent("in-rust-we-trust")
+        .build()
+        .unwrap();
+    let mut orders_sv = factory.orders_service();
+    let orders = orders_sv
+        .get_orders(invest_api_rust_sdk::contracts::GetOrdersRequest {
+            account_id,
+            advanced_filters: None,
+        })
+        .await;
+    println!("{orders:#?}");
 }
